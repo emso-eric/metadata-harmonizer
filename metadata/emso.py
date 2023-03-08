34d77b6a736e1994865506f91f473896af349fc5
Enoc Martínez <enoc.martinez@upc.edu>
2023-03-08 16:25:37 +0100

    speed up download with threads

--- metadata/emso.py
+++ metadata/emso.py
@@ -14,6 +14,9 @@ import ssl
 import rich
 import pandas as pd
 import json
+import concurrent.futures as futures
+
+
 
 emso_version = "develop"
 #emso_version = "v0.1"
@@ -25,9 +28,10 @@ emso_codes_url = f"https://gitlab.emso.eu/Martinez/emso-metadata-specification/-
 sdn_vocab_p01 = "https://vocab.nerc.ac.uk/collection/P01/current/?_profile=nvs&_mediatype=application/ld+json"
 sdn_vocab_p02 = "https://vocab.nerc.ac.uk/collection/P01/current/?_profile=nvs&_mediatype=application/ld+json"
 sdn_vocab_p06 = "https://vocab.nerc.ac.uk/collection/P06/current/?_profile=nvs&_mediatype=application/ld+json"
-sdn_vocab_l22 = "https://vocab.nerc.ac.uk/collection/L22/current/?_profile=nvs&_mediatype=application/ld+json"
 sdn_vocab_l05 = "https://vocab.nerc.ac.uk/collection/L22/current/?_profile=nvs&_mediatype=application/ld+json"
 sdn_vocab_l06 = "https://vocab.nerc.ac.uk/collection/L22/current/?_profile=nvs&_mediatype=application/ld+json"
+sdn_vocab_l22 = "https://vocab.nerc.ac.uk/collection/L22/current/?_profile=nvs&_mediatype=application/ld+json"
+sdn_vocab_l35 = "https://vocab.nerc.ac.uk/collection/L35/current/?_profile=nvs&_mediatype=application/ld+json"
 standard_names = "https://vocab.nerc.ac.uk/standard_name/?_profile=nvs&_mediatype=application/ld+json"
 
 edmo_codes = "https://edmo.seadatanet.org/sparql/sparql?query=SELECT%20%3Fs%20%3Fp%20%3Fo%20WHERE%20%7B%20%0D%0A%0" \
@@ -81,31 +85,58 @@ def process_markdown_file(file) -> (dict, dict):
     return tables
 
 
+def threadify(arg_list, handler, max_threads=10, text: str = "progress..."):
+    """
+    Splits a repetitive task into several threads
+    :param arg_list: each element in the list will crate a thread and its contents passed to the handler
+    :param handler: function to be invoked by every thread
+    :param max_threads: Max threads to be launched at once
+    :return: a list with the results (ordered as arg_list)
+    """
+    index = 0  # thread index
+    with futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        threads = []  # empty thread list
+        results = []  # empty list of thread results
+        for args in arg_list:
+            # submit tasks to the executor and append the tasks to the thread list
+            threads.append(executor.submit(handler, *args))
+            index += 1
+
+        # wait for all threads to end
+        with rich.progress.Progress() as progress:  # Use Progress() to show a nice progress bar
+            task = progress.add_task(text, total=index)
+            for future in futures.as_completed(threads):
+                future_result = future.result()  # result of the handler
+                results.append(future_result)
+                progress.update(task, advance=1)
+
+
 def download_files(tasks, force_download=False):
     if len(tasks) == 1:
         return None
 
     rich.print("Downloading files...")
+    args = []
     for url, file, name in tasks:
         if os.path.isfile(file) and not force_download:
-            rich.print(f"     [dark_grey]{name} already downloaded")
+            rich.print(f"    [dark_grey]{name} already downloaded")
         else:
-            rich.print(f"    downloading [cyan]'{name}'[/cyan]...", end="")
-            urllib.request.urlretrieve(url, file)
-            rich.print("[green]done!")
+            rich.print(f"    downloading [cyan]'{name}'[/cyan]...")
+            #urllib.request.urlretrieve(url, file)
+            args.append((url, file))
 
-    rich.print("[green]All files downloaded!")
+    threadify(args, urllib.request.urlretrieve)
 
 
-def get_sdn_jsonld_ids(file):
-    with open(file) as f:
+def get_sdn_jsonld_ids(file, label):
+    with rich.progress.open(file, "rb", description=f"loading {label}") as f:
         data = json.load(f)
-
     ids = []
     for element in data["@graph"]:
         if "identifier" in element.keys():
             ids.append(element["identifier"])
     return ids
+
 
 
 def get_standard_names(file):
@@ -146,12 +177,13 @@ class EmsoMetadata:
         self.emso_metadata_file = os.path.join(self.__folder, "EMSO_metadata.md")
         self.oceansites_file = os.path.join(self.__folder, "OceanSites_codes.md")
         self.emso_sites_file = os.path.join(self.__folder, "EMSO_codes.md")
-        self.sdn_vocab_p01_file = os.path.join(self.__folder, "sdn_vocab_p01.json")
-        self.sdn_vocab_p02_file = os.path.join(self.__folder, "sdn_vocab_p02.json")
-        self.sdn_vocab_p06_file = os.path.join(self.__folder, "sdn_vocab_p06.json")
-        self.sdn_vocab_l05_file = os.path.join(self.__folder, "sdn_vocab_l05.json")
-        self.sdn_vocab_l06_file = os.path.join(self.__folder, "sdn_vocab_l06.json")
-        self.sdn_vocab_l22_file = os.path.join(self.__folder, "sdn_vocab_l22.json")
+        sdn_vocab_p01_file = os.path.join(self.__folder, "sdn_vocab_p01.json")
+        sdn_vocab_p02_file = os.path.join(self.__folder, "sdn_vocab_p02.json")
+        sdn_vocab_p06_file = os.path.join(self.__folder, "sdn_vocab_p06.json")
+        sdn_vocab_l05_file = os.path.join(self.__folder, "sdn_vocab_l05.json")
+        sdn_vocab_l06_file = os.path.join(self.__folder, "sdn_vocab_l06.json")
+        sdn_vocab_l22_file = os.path.join(self.__folder, "sdn_vocab_l22.json")
+        sdn_vocab_l35_file = os.path.join(self.__folder, "sdn_vocab_l35.json")
         self.standard_names_file = os.path.join(self.__folder, "standard_names.json")
         self.edmo_codes_file = os.path.join(self.__folder, "edmo_codes.json")
 
@@ -159,12 +191,13 @@ class EmsoMetadata:
             [emso_metadata_url, self.emso_metadata_file, "EMSO metadata"],
             [oceansites_codes_url, self.oceansites_file, "OceanSites"],
             [emso_codes_url, self.emso_sites_file, "EMSO codes"],
-            [sdn_vocab_p01, self.sdn_vocab_p01_file, "SDN Vocab P01"],
-            [sdn_vocab_p02, self.sdn_vocab_p02_file, "SDN Vocab P02"],
-            [sdn_vocab_p06, self.sdn_vocab_p06_file, "SDN Vocab P06"],
-            [sdn_vocab_l05, self.sdn_vocab_l05_file, "SDN Vocab L05"],
-            [sdn_vocab_l06, self.sdn_vocab_l06_file, "SDN Vocab L06"],
-            [sdn_vocab_l22, self.sdn_vocab_l22_file, "SDN Vocab L22"],
+            [sdn_vocab_p01, sdn_vocab_p01_file, "SDN Vocab P01"],
+            [sdn_vocab_p02, sdn_vocab_p02_file, "SDN Vocab P02"],
+            [sdn_vocab_p06, sdn_vocab_p06_file, "SDN Vocab P06"],
+            [sdn_vocab_l05, sdn_vocab_l05_file, "SDN Vocab L05"],
+            [sdn_vocab_l06, sdn_vocab_l06_file, "SDN Vocab L06"],
+            [sdn_vocab_l22, sdn_vocab_l22_file, "SDN Vocab L22"],
+            [sdn_vocab_l35, sdn_vocab_l35_file, "SDN Vocab L35"],
             [standard_names, self.standard_names_file, "CF standard names"],
             [edmo_codes, self.edmo_codes_file, "EDMO codes"]
         ]
@@ -182,14 +215,15 @@ class EmsoMetadata:
         tables = process_markdown_file(self.emso_sites_file)
         self.emso_regional_facilities = tables["EMSO Regional Facilities"]
 
-        rich.print("Loading SeaDataNet vocabularies...", end="")
+        rich.print("Loading SeaDataNet vocabularies...")
         self.sdn_vocabs = {
-            "P01": get_sdn_jsonld_ids(self.sdn_vocab_p01_file),
-            "P02": get_sdn_jsonld_ids(self.sdn_vocab_p02_file),
-            "P06": get_sdn_jsonld_ids(self.sdn_vocab_p06_file),
-            "L05": get_sdn_jsonld_ids(self.sdn_vocab_l05_file),
-            "L06": get_sdn_jsonld_ids(self.sdn_vocab_l06_file),
-            "L22": get_sdn_jsonld_ids(self.sdn_vocab_l22_file)
+            "P01": get_sdn_jsonld_ids(sdn_vocab_p01_file, "P01"),
+            "P02": get_sdn_jsonld_ids(sdn_vocab_p02_file, "P02"),
+            "P06": get_sdn_jsonld_ids(sdn_vocab_p06_file, "P06"),
+            "L05": get_sdn_jsonld_ids(sdn_vocab_l05_file, "L05"),
+            "L06": get_sdn_jsonld_ids(sdn_vocab_l06_file, "L06"),
+            "L22": get_sdn_jsonld_ids(sdn_vocab_l22_file, "L22"),
+            "L35": get_sdn_jsonld_ids(sdn_vocab_l35_file, "L35")
         }
         self.standard_names = get_standard_names(self.standard_names_file)
         self.edmo_codes = get_edmo_codes(self.edmo_codes_file)
