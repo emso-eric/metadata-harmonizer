b9a8093f89f97e6f54c318445cb87a8d76c4a144
Enoc Martínez <enoc.martinez@upc.edu>
2025-05-20 13:09:13 +0200

    fixing string generation in netcdf

--- src/emso_metadata_harmonizer/metadata/waterframe.py
+++ src/emso_metadata_harmonizer/metadata/waterframe.py
@@ -363,17 +363,18 @@ class WaterFrame(LoggerSuperclass):
             # Convert station_name into an array of chars
             if len(df["platform_id"].unique()) != 1:
                 raise ValueError("Expected only one platform_id for NetCDF file!")
-            platform_id = df["platform_id"].unique()[0]
-            var[:] = np.array([c for c in platform_id.ljust(strlen)], 'S1')
+            platform_id = str(df["platform_id"].unique()[0])
+            var[:] = np.array([c for c in platform_id.ljust(strlen)])
             self.populate_var_metadata("platform_id", var)
             var.setncattr("cf_role", "timeseries_id")
 
-            # for sensor in self.sensors:
-            #     sensor_id = sensor["sensor_id"]
-            #     self.info(f"Creating variable to store '{sensor_id}' metadata")
-            #     sensor_var = ncfile.createVariable(sensor_id, "S1", ("strlen",))
-            #     var[:] = np.array(sensor_id.ljust(strlen))
-            #     self.__metadata_from_dict(sensor_var, sensor)
+            for sensor in self.sensors:
+                sensor_id = sensor["sensor_id"]
+                self.info(f"Creating variable to store '{sensor_id}' metadata")
+                sensor_var = ncfile.createVariable(sensor_id, "S1", ("strlen",))
+
+                sensor_var[:] = np.array([c for c in sensor_id.ljust(strlen)])
+                self.__metadata_from_dict(sensor_var, sensor)
 
             for varname in variables:
                 var = ncfile.createVariable(varname, "double", ("time", "depth"), zlib=False)
@@ -393,7 +394,6 @@ class WaterFrame(LoggerSuperclass):
                     # convert flags to unsigned one byte
                     self.vocabulary[varname_qc]["flag_values"] = self.flag_values
                     self.populate_var_metadata(varname_qc, var_qc)
-
 
             # Set global attributes
             for key, value in self.metadata.items():
@@ -543,57 +543,3 @@ def merge_waterframes(waterframes):
             wf.metadata["date_created"] = now
         wf.metadata["date_modified"] = wf.metadata["date_created"]
     return wf
-
-
-
-#
-# def merge_waterframes(waterframes) -> WaterFrame:
-#     """
-#     Combine all WaterFrames into a single waterframe. Both data and metadata are consolidated into a single
-#     structure
-#     """
-#     dataframes = []  # list of dataframes
-#     global_attr = []  # list of dict containing global attributes
-#     variables_attr = {}  # dict all the variables metadata
-#     for wf in waterframes:
-#         df = wf.data
-#         # setting time as the index
-#         df = df.set_index("time")
-#         df = df.sort_index(ascending=True)
-#         df["sensor_id"] = wf.metadata["$sensor_id"]
-#         if not df.empty:
-#             dataframes.append(df)
-#         global_attr.append(wf.metadata)
-#         for varname, varmeta in wf.vocabulary.items():
-#             if varname not in variables_attr.keys():
-#                 variables_attr[varname] = [varmeta]  # list of dicts with metadata
-#             else:
-#                 variables_attr[varname].append(varmeta)
-#
-#     df = pd.concat(dataframes)  # Consolidate data in a single dataframe
-#     df = df.sort_index(ascending=True)  # sort by date
-#     df = df.reset_index()  # get back to numerical index
-#
-#     # Consolidating Global metadata, the position in the array is the priority
-#     global_meta = {}
-#     for g in reversed(global_attr):  # loop backwards, last element has lower priority
-#         global_meta = merge_dicts(g, global_meta)
-#
-#     variable_meta = {}
-#     for varname, varmeta in variables_attr.items():
-#         variable_meta[varname] = consolidate_metadata(varmeta)
-#
-#     wf = WaterFrame(df, global_meta, variable_meta)
-#     wf = autofill_waterframe_coverage(wf)  # update the coordinates max/min in metadata
-#
-#     # Add versioning info
-#     now = pd.Timestamp.now(tz="utc").strftime(iso_time_format)
-#     if len(waterframes) > 1:
-#         # New waterframe
-#         wf.metadata["date_created"] = now
-#         wf.metadata["date_modified"] = now
-#     else:  # just update the date_modified
-#         if "date_created" not in wf.metadata.keys():
-#             wf.metadata["date_created"] = now
-#         wf.metadata["date_modified"] = wf.metadata["date_created"]
-#     return wf
