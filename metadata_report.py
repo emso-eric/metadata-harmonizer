7d62119ad7b134560b7a84eecf871819a5e63309
Enoc Martínez <enoc.martinez@upc.edu>
2026-05-15 14:01:35 +0200

    improving doc on --csv

--- metadata_report.py
+++ metadata_report.py
@@ -18,12 +18,14 @@ if __name__ == "__main__":
     argparser.add_argument("target", type=str, help="ERDDAP service URL, NetCDF file or JSON metadata file", default="", nargs='?')
     argparser.add_argument("-v", "--verbose", action="store_true", help="Shows more info")
     argparser.add_argument("-o", "--output", type=str, help="file to store the summary of ALL datasets (multiple datasets)", default="")
-    argparser.add_argument("-c", "--csv",  type=str, help="file to store CSV the report of ONE dataset (individual datasets)", default="")
+    argparser.add_argument("-c", "--csv",  type=str, help="Folder to store CSV files with the report of each dataset", default="")
     argparser.add_argument("-i", "--ignore-ok", action="store_true", help="do not show tests with positive outcome")
     argparser.add_argument("-V", "--variables", nargs="+", help="Run test only for a variable subset", default=[])
     argparser.add_argument("--specs", type=str, help="Use this file as EMSO Metadata specifications source (use only for development)", default="")
     argparser.add_argument("-l", "--log-level", type=str, help="Setting log level (debug, info, warn, error or critical)", required=False, default="")
     argparser.add_argument("-k", "--keywords", action="store_true", help="Check the keywords")
+    argparser.add_argument("-s", "--summary", action="store_true", help="Prints a summary of all datasets")
+    argparser.add_argument("-q", "--quiet", action="store_true", help="Do not print the results in the stdout")
 
     args = argparser.parse_args()
 
@@ -40,6 +42,9 @@ if __name__ == "__main__":
         specifications=args.specs,
         variables=args.variables,
         ignore_ok=args.ignore_ok,
-        csv=args.csv,
+        csv_folder=args.csv,
         keywords=args.keywords,
+        summary=args.summary,
+        quiet=args.quiet
+
     )
